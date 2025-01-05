499a507e44c4a822f19be08bb846f68520e82099
whgiles <huntergiles2@gmail.com>
2025-01-04 20:45:58 -0500

    save

--- scripts/add-data.py
+++ scripts/add-data.py
@@ -1,8 +1,11 @@
+import json
+
 import pandas as pd
 import requests
 
-API_BASE_URL = "localhost:3000"
-AUTH_TOKEN = ""
+API_BASE_URL = "http://localhost:3000"
+AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0MmUzZTQyNC02OTVjLTQ3MGMtYWFlZi03ZTIwZWM3YmMwMzIiLCJ1c2VybmFtZSI6InN1bm55RDEyMyIsInVzZXJJZCI6IjQyZTNlNDI0LTY5NWMtNDcwYy1hYWVmLTdlMjBlYzdiYzAzMiIsImlhdCI6MTczNjAzOTg3OSwiZXhwIjoxNzM2MTI2Mjc5fQ.5f8oGQXCX3d8dYun_qb3yDVQ4RQW1ZH-YHoi4fO6-uw"
+OUTPUT_FILE_PATH = "./expense_output.json"
 CSV_FILE_PATH = "./transformed_finance_data.csv"
 # NAME,YEAR,CATEGORY,SUB_CATEGORY,MONTH,VALUE
 
@@ -50,15 +53,8 @@ def create_reports():
             )
 
 
-def insert_data():
+def insert_expense_data():
     df = pd.read_csv(CSV_FILE_PATH)
-
-    income = {
-        "reportId": None,
-        "employeeBenefit": [],
-        "paycheck": [],
-        "otherIncome": [],
-    }
 
     expense = {
         "reportId": None,
@@ -69,36 +65,55 @@ def insert_data():
     }
 
     for _, row in df.iterrows():
-        # TODO: add reportId
-        if row["CATEGORY"] == "EXPENSE" or row["SUB_CATEGORY"] == "BANK":
+        if "VALUE" not in row or pd.isna(row["VALUE"]):
+            print("Missing VALUE field. Exiting loop.")
+            continue
 
-            if row["SUB_CATEGORY"] == "UTILITY":
-                payload = {"amount": row["VALUE"], "type": row["NAME"]}
-                expense["utilities"].append(payload)
+        if not (row["CATEGORY"] == "EXPENSE" or row["CATEGORY"] == "BANK"):
+            continue
 
-            if row["SUB_CATEGORY"] == "RENT":
-                payload = {"amount": row["VALUE"], "rentor": "MAA Peachtree Hills"}
-                expense["rent"].append(payload)
+        raw = requests.get(
+            f"{API_BASE_URL}/expense/expense-report/{row['MONTH']}/{row['YEAR']}"
+        )
+        response_json = raw.json()
+        expense["reportId"] = response_json.get("reportId")
 
-            if row["SUB_CATEGORY"] == "CARD":
-                # TODO: account type should be variable
-                payload = {
-                    "bankName": row["NAME"],
-                    "accountType": "CREDIT",
-                    "amount": row["VALUE"],
-                    "isPayed": True,
-                }
-                expense["cardEndOfMonthStatement"].append(payload)
+        if row["SUB_CATEGORY"] == "UTILITY":
+            payload = {"amount": row["VALUE"], "type": row["NAME"]}
+            expense["utilities"].append(payload)
+        if row["SUB_CATEGORY"] == "RENT":
+            payload = {"amount": row["VALUE"], "rentor": "MAA Peachtree Hills"}
+            expense["rent"].append(payload)
+        if row["SUB_CATEGORY"] == "CARD":
+            # TODO: account type should be variable
+            payload = {
+                "bankName": row["NAME"],
+                "accountType": "CREDIT",
+                "amount": row["VALUE"],
+                "isPayed": True,
+            }
+            expense["cardEndOfMonthStatement"].append(payload)
+        if row["SUB_CATEGORY"] == "OTHER":
+            # TODO: datePayed should be variable
+            payload = {
+                "datePayed": "2024-02-10T10:07:58.767Z",
+                "type": row["NAME"],
+                "amount": row["VALUE"],
+            }
+            expense["otherExpense"].append(payload)
 
-            if row["SUB_CATEGORY"] == "OTHER":
-                # TODO: datePayed should be variable
-                payload = {
-                    "datePayed": "2024-02-10T10:07:58.767Z",
-                    "type": row["NAME"],
-                    "amount": row["VALUE"],
-                }
-
-    print(expense)
+    # with open(OUTPUT_FILE_PATH, "w") as outfile:
+    #     json.dump(expense, outfile, indent=4)
 
 
-insert_data()
+def insert_ncome_data():
+    df = pd.read_csv(CSV_FILE_PATH)
+    income = {
+        "reportId": None,
+        "employeeBenefit": [],
+        "paycheck": [],
+        "otherIncome": [],
+    }
+
+
+insert_expense_data()
