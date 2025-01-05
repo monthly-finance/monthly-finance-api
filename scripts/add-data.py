156015f3136e20453e0ee1304f4779dd0d73870a
whgiles <huntergiles2@gmail.com>
2025-01-04 23:19:22 -0500

    feat: added decent script to insert test data

--- scripts/add-data.py
+++ scripts/add-data.py
@@ -4,10 +4,15 @@ import pandas as pd
 import requests
 
 API_BASE_URL = "http://localhost:3000"
-AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0MmUzZTQyNC02OTVjLTQ3MGMtYWFlZi03ZTIwZWM3YmMwMzIiLCJ1c2VybmFtZSI6InN1bm55RDEyMyIsInVzZXJJZCI6IjQyZTNlNDI0LTY5NWMtNDcwYy1hYWVmLTdlMjBlYzdiYzAzMiIsImlhdCI6MTczNjAzOTg3OSwiZXhwIjoxNzM2MTI2Mjc5fQ.5f8oGQXCX3d8dYun_qb3yDVQ4RQW1ZH-YHoi4fO6-uw"
+AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxN2I2YzgwNy1hM2FlLTQyM2EtODY3OC0wNzIzNzQ3YWQ5NjEiLCJ1c2VybmFtZSI6InN1bm55RDEyMyIsInVzZXJJZCI6IjE3YjZjODA3LWEzYWUtNDIzYS04Njc4LTA3MjM3NDdhZDk2MSIsImlhdCI6MTczNjA0NTM0MSwiZXhwIjoxNzM2MTMxNzQxfQ.HIRzs0WX3GxRyLaZ1IbQeLT4oVtzKLLhi4QzR9xDRWY"
 OUTPUT_FILE_PATH = "./expense_output.json"
 CSV_FILE_PATH = "./transformed_finance_data.csv"
 # NAME,YEAR,CATEGORY,SUB_CATEGORY,MONTH,VALUE
+
+headers = {
+    "Authorization": f"Bearer {AUTH_TOKEN}",
+    "Content-Type": "application/json",
+}
 
 
 def create_reports():
@@ -17,11 +22,6 @@ def create_reports():
     """
     df = pd.read_csv(CSV_FILE_PATH)
     unique_combinations = df[["MONTH", "YEAR"]].drop_duplicates()
-
-    headers = {
-        "Authorization": f"Bearer {AUTH_TOKEN}",
-        "Content-Type": "application/json",
-    }
 
     for _, row in unique_combinations.iterrows():
         month, year = row["MONTH"], row["YEAR"]
@@ -43,7 +43,7 @@ def create_reports():
             )
 
         income_response = requests.post(
-            f"{api_base_url}/income/income-report", json=income_payload, headers=headers
+            f"{API_BASE_URL}/income/income-report", json=income_payload, headers=headers
         )
         if income_response.status_code == 201:
             print(f"Income report created for {month} {year}.")
@@ -66,17 +66,18 @@ def insert_expense_data():
 
     for _, row in df.iterrows():
         if "VALUE" not in row or pd.isna(row["VALUE"]):
-            print("Missing VALUE field. Exiting loop.")
+            print("Missing VALUE field. Skipping row.")
             continue
 
         if not (row["CATEGORY"] == "EXPENSE" or row["CATEGORY"] == "BANK"):
             continue
 
         raw = requests.get(
-            f"{API_BASE_URL}/expense/expense-report/{row['MONTH']}/{row['YEAR']}"
+            f"{API_BASE_URL}/expense/expense-report/{row['MONTH']}/{row['YEAR']}",
+            headers=headers,
         )
         response_json = raw.json()
-        expense["reportId"] = response_json.get("reportId")
+        expense["reportId"] = response_json.get("id")
 
         if row["SUB_CATEGORY"] == "UTILITY":
             payload = {"amount": row["VALUE"], "type": row["NAME"]}
@@ -102,18 +103,75 @@ def insert_expense_data():
             }
             expense["otherExpense"].append(payload)
 
-    # with open(OUTPUT_FILE_PATH, "w") as outfile:
-    #     json.dump(expense, outfile, indent=4)
+        response = requests.post(
+            f"{API_BASE_URL}/expense/expense-report/insert",
+            json=expense,
+            headers=headers,
+        )
+        if response.status_code == 201:
+            print("Expense data inserted successfully.")
+        else:
+            print(f"Failed to insert expense data: {response.text}")
 
 
-def insert_ncome_data():
+def insert_income_data():
     df = pd.read_csv(CSV_FILE_PATH)
-    income = {
-        "reportId": None,
-        "employeeBenefit": [],
-        "paycheck": [],
-        "otherIncome": [],
-    }
+
+    for _, row in df.iterrows():
+
+        if "VALUE" not in row or pd.isna(row["VALUE"]):
+            print("Missing VALUE field. Skipping row.")
+            continue
+
+        if row["CATEGORY"] != "INCOME":
+            continue
+
+        income = {
+            "reportId": None,
+            "employeeBenefit": [],
+            "paycheck": [],
+            "otherIncome": [],
+        }
+
+        raw = requests.get(
+            f"{API_BASE_URL}/income/income-report/{row['MONTH']}/{row['YEAR']}",
+            headers=headers,
+        )
+        response_json = raw.json()
+        income["reportId"] = response_json.get("id")
+
+        if row["SUB_CATEGORY"] == "EMPLOYEE_BENEFIT":
+            payload = {
+                "datePayed": "2024-02-10T10:07:58.767Z",  # TODO: Make variable
+                "amount": row["VALUE"],
+                "type": row["NAME"],
+            }
+            income["employeeBenefit"].append(payload)
+
+        if row["SUB_CATEGORY"] == "WAGE":
+            payload = {
+                "datePayed": "2024-02-10T10:07:58.767Z",  # TODO: Make variable
+                "amount": row["VALUE"],
+            }
+            income["paycheck"].append(payload)
+
+        if row["SUB_CATEGORY"] == "OTHER":
+            payload = {
+                "datePayed": "2024-02-10T10:07:58.767Z",  # TODO: Make variable
+                "type": row["NAME"],
+                "amount": row["VALUE"],
+            }
+            income["otherIncome"].append(payload)
+        print(income)
+        response = requests.post(
+            f"{API_BASE_URL}/income/income-report/insert", json=income, headers=headers
+        )
+        if response.status_code == 201:
+            print("Income data inserted successfully.")
+        else:
+            print(f"Failed to insert income data: {response.text}")
 
 
+# create_reports()
 insert_expense_data()
+# insert_income_data()
